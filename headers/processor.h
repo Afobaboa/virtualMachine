9deae50059471939261573b2c68574f9ec975df9
Afobaboa <gushind1984@gmail.com>
2024-10-18 18:34:12 +0300

    added JUMP_ARGC

--- headers/processor.h
+++ headers/processor.h
@@ -47,13 +47,14 @@ const size_t maxCmdLength = 32;
  * Arguments' count of each command of virtualMachine.
  */
 typedef int argc_t;
-const argc_t PUSH_ARGC  = 1;
-const argc_t ADD_ARGC   = 0;
-const argc_t SUB_ARGC   = 0;
-const argc_t DIV_ARGC   = 0;
-const argc_t MUL_ARGC   = 0;
-const argc_t IN_ARGC    = 0;
-const argc_t OUT_ARGC   = 0;
+const argc_t PUSH_ARGC = 1;
+const argc_t ADD_ARGC  = 0;
+const argc_t SUB_ARGC  = 0;
+const argc_t DIV_ARGC  = 0;
+const argc_t MUL_ARGC  = 0;
+const argc_t IN_ARGC   = 0;
+const argc_t OUT_ARGC  = 0;
+const argc_t JMP_ARGC  = 1;
 
 const size_t maxCmdArgc = 4;
 
