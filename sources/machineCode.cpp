9d1e9ade1a70411d9c9ec30cad0d47a24f12df17
Afobaboa <gushind1984@gmail.com>
2024-10-19 13:31:34 +0300

    Added interface of new functions

--- sources/machineCode.cpp
+++ sources/machineCode.cpp
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "machineCode.h"
+#include "logPrinter.h"
 
 
 //--------------------------------------------------------------------------------------------------
@@ -40,4 +42,21 @@ codeStatus_t MachineCodeAddInstruction(MachineCode* machineCode, const instructi
 codeStatus_t MachineCodeJump(MachineCode* machineCode, const size_t instructionNum)
 {
 
+}
+
+
+bool MachineCodeWriteToFile(MachineCode* machineCode, char* fileName)
+{
+    FILE* file = fopen(fileName, "wb");
+    if (file == NULL)
+    {
+        ColoredPrintf(RED, "Can't write machine code to %s.\n", fileName);
+        return false;
+    }
+
+    fwrite(&(machineCode->instructionCount), sizeof(size_t), 1, file);
+    fwrite(machineCode->code, sizeof(instruction_t), machineCode->instructionCount, file);
+
+    fclose(file);
+    return true;
 }
